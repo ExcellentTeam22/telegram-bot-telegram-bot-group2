f566c5b13eb37b31fb32e676020eb06725fab0c6
Yarden Gahan <yardengahan@gmail.com>
2022-08-18 12:48:25 +0300

    finished excersize pt1

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import math
+from re import split
+
 import requests as requests
 from flask import Flask, Response, request
 
@@ -17,10 +20,53 @@ def sanity(): return "Server is running"
 @app.route('/message', methods=["POST"])
 def handle_message():
     print("got message")
-    chat_id = request.get_json()['message']['chat']['id']
+    json_got = request.get_json()
+    chat_id = json_got['message']['chat']['id']
+    result = None
+    command = (json_got['message']['text']).split()[0]
+    user_input = (json_got['message']['text']).split()[1]
+
+    if command == "/palindrome":
+        result = palindrome(user_input)
+
+    if command == "/factorial":
+        result = factorial(user_input)
+
+    if command == "/sqrt":
+        result = sqrt(user_input)
+
     res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}"
-                       .format(TOKEN, chat_id, "Got it"))
+                       .format(TOKEN, chat_id, result))
     return Response("success")
+
+
+def palindrome(num):
+    result = True if num == num[::-1] else False
+    return result
+
+
+def factorial(num):
+    num = int(num)
+    i = 1;
+    while True:
+        if num % i == 0:
+            num //= i;
+        else:
+            break;
+        i += 1;
+
+    if num == 1:
+        return True;
+    else:
+        return False;
+
+
+def sqrt(num):
+    num = int(num)
+    if num < 0:
+        return False
+    else:
+        return math.sqrt(num).is_integer()
 
 
 if __name__ == '__main__':
