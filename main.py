974cd40269da1f978bedfb4800930f505d03b258
Eitan Abraham <eitanabraham@gmail.com>
2022-08-18 11:32:16 +0300

    removed notes

--- main.py
+++ main.py
@@ -4,21 +4,19 @@ from flask import Flask, Response, Request
 app = Flask(__name__)
 
 TOKEN = '5350437246:AAGKH4QokVbN6u0-FEduXOiTVNrBbjS1GqE'
-TELEGRAM_INIT_WEBHOOK_URL ='https://api.telegram.org/bot{}/setWebhook?url=https://45bd-82-80-173-170.eu.ngrok.io/message'.format(TOKEN)
+TELEGRAM_INIT_WEBHOOK_URL ='https://api.telegram.org/bot{}/setWebhook?url=https://d8c2-2a02-14f-3-da03-b9a9-1b99-d6b1-3f46.eu.ngrok.io/message'.format(TOKEN)
 requests.get(TELEGRAM_INIT_WEBHOOK_URL)
 
-# 'https://api.telegram.org/bot5350437246:AAGKH4QokVbN6u0-FEduXOiTVNrBbjS1GqE/setWebhook?url=https://008d-5-28-184-223.eu.ngrok.io/message'
+@app.route('/sanity')
+def sanity():return "Server is running"
 
-# @app.route('/sanity')
-# def sanity():return "Server is running"
-
-# @app.route('/message', methods=["POST"])
-# def handle_message():
-#     print("got message")
-#     chat_id = Request.get_json()['message']['chat']['id']
-#     res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'"
-#                        .format(TOKEN, chat_id, "Got it!!!! :) "))
-#     return Response("success")
+@app.route('/message', methods=["POST"])
+def handle_message():
+    print("got message")
+    chat_id = Request.get_json()['message']['chat']['id']
+    res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'"
+                       .format(TOKEN, chat_id, "Got it!!!! :) "))
+    return Response("success")
 
 
 if __name__ == '__main__':
