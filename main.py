ae5d292f46f37e21096e3583fa25d6d3738b5535
Yarden Gahan <yardengahan@gmail.com>
2022-08-18 11:51:37 +0300

    working main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 TOKEN = '5350437246:AAGKH4QokVbN6u0-FEduXOiTVNrBbjS1GqE'
 TELEGRAM_INIT_WEBHOOK_URL = 'https://api.telegram.org/bot{}/setWebhook?url=' \
-                            ' https://807f-82-80-173-170.eu.ngrok.io\/message'. \
+                            'https://3b2f-82-80-173-170.ngrok.io/message'. \
     format(TOKEN)
 requests.get(TELEGRAM_INIT_WEBHOOK_URL)
 
@@ -18,7 +18,7 @@ def sanity(): return "Server is running"
 def handle_message():
     print("got message")
     chat_id = request.get_json()['message']['chat']['id']
-    res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'"
+    res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}"
                        .format(TOKEN, chat_id, "Got it"))
     return Response("success")
 
